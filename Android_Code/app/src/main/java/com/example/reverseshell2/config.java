2bd3c4f710f7b34c0832c60bcf69ccfb0c8b2863
RT <140252584+X-Xspida@users.noreply.github.com>
2023-10-29 00:10:50 +0500

    Update config.java

--- Android_Code/app/src/main/java/com/example/reverseshell2/config.java
+++ Android_Code/app/src/main/java/com/example/reverseshell2/config.java
@@ -1,7 +1,7 @@
 package com.example.reverseshell2;
 
 public class config {
-    public static String IP = "192.168.0.105";
-    public static String port = "8888";
+    public static String IP = "192.168.131.128";
+    public static String port = "8080";
     public static boolean icon = true;
 }
